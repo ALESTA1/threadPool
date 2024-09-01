61af7d95ea813ab1fcc0c6256c0f44db765f2b08
Raghav <83813503+ALESTA1@users.noreply.github.com>
2024-09-02 00:13:19 +0530

    Update threadPool.cpp

--- threadPool.cpp
+++ threadPool.cpp
@@ -1,54 +1,117 @@
-#include<bits/stdc++.h>
-#include<mutex>
+#include <bits/stdc++.h>
+
+#include <mutex>
+
 #include <vector>
+
 #include <condition_variable>
+
 #include <iostream>
+
 #include <functional>
+
 #include <queue>
 
 using namespace std;
 
-class threadPool{
+class ThreadPool {
 
-    public:
-    threadPool() = delete;
-ThreadPool(int _threads) {
+  public:
 
+    ThreadPool(int _threads): stop(false) {
 
-    for (int i = 0; i < _threads; ++i) {
-        threads.emplace_back([this] { executeTask(); }); // Create and add thread directly
-    }
-}
+      for (int i = 0; i < _threads; ++i) {
 
+        threads.emplace_back([this] {
+          executeTask();
+        });
 
-    void addTask(function<void()> task){
-
-        unique_lock<mutex> lock(m);
-        functionQueue.push(task);
+      }
 
     }
-    void executeTask(){
 
-        while(true){
+    ~ThreadPool() {
+      {
 
-            {
-                unique_lock<mutex> lock(m);
-                cv.wait(lock,[]{return functionQueue.size(})
+        unique_lock < mutex > lock(m);
+        stop = true;
 
+      }
+      cv.notify_all();
+      for (auto & thread: threads) {
 
-            }
+        if (thread.joinable()) {
+
+          thread.join();
+
         }
+
+      }
+
     }
 
-    private:
+  void addTask(function < void() > task) {
 
-    vector<std::thread> threads;
-    condition_variable cv;
-    mutex m;
-    std::queue<std::function<void()>> functionQueue;
+    {
+
+      unique_lock < mutex > lock(m);
+
+      functionQueue.push(task);
+
+    }
+
+    cv.notify_one();
+
+  }
+
+  private:
+
+    void executeTask() {
+
+      while (true) {
+
+        function < void() > task;
+
+        {
+
+          unique_lock < mutex > lock(m);
+
+          cv.wait(lock, [this] {
+            return stop || !functionQueue.empty();
+          });
+
+          if (stop && functionQueue.empty()) {
+
+            return;
+
+          }
+
+          task = functionQueue.front();
+
+          functionQueue.pop();
+
+        }
+
+        task();
+
+      }
+
+    }
+
+  vector < thread > threads;
+
+  condition_variable cv;
+
+  mutex m;
+
+  queue < function < void() >> functionQueue;
+
+  bool stop;
 
 };
-int main(){
 
-    return 0;
+int main() {
+
+  return 0;
+
 }
